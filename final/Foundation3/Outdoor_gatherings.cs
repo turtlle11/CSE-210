6e8c72c1b1af209ff05caa2d36a475aa34b3515a
turtlle11 <misha182.tihomirov@gmail.com>
2024-07-19 16:30:40 -0600

    f3

--- final/Foundation3/Outdoor_gatherings.cs
+++ final/Foundation3/Outdoor_gatherings.cs
@@ -8,7 +8,7 @@ class Outdoor_gatherings_event : Event
     public void Full_Event_Details()
     {
         Console.WriteLine($"Full {_type_of_event} event details\n");
-        Console.WriteLine($"Title: {_event_title}\n\nDescription: {_event_description}\n\nDate: {_event_date}\n\nTime: {_event_time}\n\nStatement of the weather for this day: {_statement_of_the_weather} Address: {_event_address.Get_Full_Address()}");
+        Console.WriteLine($"Title: {_event_title}\n\nDescription: {_event_description}\n\nDate: {_event_date}\n\nTime: {_event_time}\n\nStatement of the weather for this day: {_statement_of_the_weather}\n\n Address: {_event_address.Get_Full_Address()}");
     }
 
 
